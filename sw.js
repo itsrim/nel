7ba2f58358d0547245a33f7ab690759fe844da6d
rim <toto31@yopmail.com>
2026-04-16 12:14:40 +0200

    Updates

--- sw.js
+++ sw.js
@@ -1214,6 +1214,6 @@ function te(a) {
 function se(a, e) {
   te(a), ee(e);
 }
-se([{"revision":"11415c72e9880d6c4d2cbfc824eef85a","url":"index.html"},{"revision":null,"url":"assets/workbox-window.prod.es5-vqzQaGvo.js"},{"revision":null,"url":"assets/index-DyexI_Wm.css"},{"revision":null,"url":"assets/index--xQfFxdo.js"},{"revision":"d6be51c1db2ba1a49859a26e5e9c93d7","url":"manifest.webmanifest"}]);
+se([{"revision":"0cdbd4a5caadfae5f93ebed8a0fcd73b","url":"index.html"},{"revision":null,"url":"assets/workbox-window.prod.es5-vqzQaGvo.js"},{"revision":null,"url":"assets/index-BeB4OTda.css"},{"revision":null,"url":"assets/index-BHPjjFhT.js"},{"revision":"d6be51c1db2ba1a49859a26e5e9c93d7","url":"manifest.webmanifest"}]);
 self.addEventListener("fetch", (a) => {
 });
